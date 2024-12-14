575d437c99b9bbc512fc8c4796a9711d11147161
NuriyaSharopova <madinasharopova308@gmail.com>
2024-12-14 20:39:08 +0500

    o'zgartirildi

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React from 'react'
 import Delivery from './Delivery/Delivery'
 import Feature from './Features/Feature'
 import Hero from './Hero/Hero'
@@ -8,28 +8,9 @@ import Newsletter from './Newsletter/Newsletter'
 import PopularBurger from './PopularBurger/PopularBurger'
 import Reservation from './Reservation/Reservation'
 import Team from './Team/Team'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
 
 
 const Home = () => {
-
-  useEffect(() => {
-
-    const initAOS = async () => { 
-      await import("aos")
-      AOS.init({
-        duration:1000,
-        easing:'ease',
-        once:true,
-        anchorPlacement:"top-center"
-      })
-    }
-    initAOS()
-    
-
-  }, [])
-
 
   return (
     <div className='overflow-hidden'>
@@ -41,7 +22,6 @@ const Home = () => {
       <Reservation />
       <Newsletter />
     </div>
-
   )
 }
 
