3356b6a2933d9fb17cf97f39c5d5fac624ca1501
NuriyaSharopova <madinasharopova308@gmail.com>
2025-01-07 20:52:32 +0500

    First commit

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import Delivery from './Delivery/Delivery'
 import Feature from './Features/Feature'
 import Hero from './Hero/Hero'
@@ -8,9 +8,24 @@ import Newsletter from './Newsletter/Newsletter'
 import PopularBurger from './PopularBurger/PopularBurger'
 import Reservation from './Reservation/Reservation'
 import Team from './Team/Team'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 
 const Home = () => {
+
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos")
+      AOS.init({
+        duration:1000,
+        easing:"ease",
+        once:true,
+        anchorPlacement:"top-center"
+      })
+    }
+    initAOS()
+  }, [])
 
   return (
     <div className='overflow-hidden'>
--- app/components/PopularBurger/PopularBurger.tsx
+++ app/components/PopularBurger/PopularBurger.tsx
@@ -4,44 +4,26 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import BurgerCard from './BurgerCard';
 
-
-//
-// const responsive = {
-//   desktop: {
-//     breakpoint: { max: 3000, min: 1324 },
-//     items: 4,
-//     slidesToSlide: 1 // optional, default to 1.
-//   },
-//   tablet: {
-//     items: 2,
-//     slidesToSlide: 1 // optional, default to 1.
-//   },
-//   mobile: {
-//     breakpoint: { max: 764, min: 0 },
-//     items: 1,
-//     slidesToSlide: 1 // optional, default to 1.
-//   }
-// };
-
 const responsive = {
-  superLargeDesktop: {
-    // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5
-  },
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    breakpoint: { max: 3000, min: 1324 },
+    items: 4,
+    slidesToSlide: 1 // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 2,
+    slidesToSlide: 1 // optional, default to 1.
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
+    breakpoint: { max: 764, min: 0 },
+    items: 1,
+    slidesToSlide: 1 // optional, default to 1.
   }
 };
+
+
+
 
 const PopularBurger = () => {
   return (
@@ -50,20 +32,6 @@ const PopularBurger = () => {
         Our Popular <span className='text-red-600'>Burgers</span>
       </h1>
       <div className='w-[80%] mt-[4rem] mx-auto '>
-        {/* <BurgerCard
-          title='Beefy Bite'
-          image='/images/b1.png'
-          reviews='6'
-          price='10.88$'
-
-        />
-         <BurgerCard
-          title='Beefy Bite'
-          image='/images/b2.png'
-          reviews='6'
-          price='10.88$'
-
-        /> */}
 
         <Carousel responsive={responsive}>
           <BurgerCard
